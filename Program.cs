dec4fec85d37db9778de129b9213b07460ffaa34
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-08-24 17:47:18 -0300

    Terminei

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Northern lights");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine ("Que seu sonho se realize... Se você tem um sonho a vida tem sentido uma razão um alvo a ser atingido ");
+            Console.WriteLine ("É uma estrela brilhando no seu céu quando se tem na alma, força e muita fé uma luta á vencer uma estrada a percorrer");
+            Console.WriteLine ("Uma história a escrever um trabalho");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine ("Com garra,força e determinação ponha os seus planos em ação");
+            Console.WriteLine ("Se você tem um sonho no seu coração a sua vida tem uma razão nada vai impedir");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Que os seus sonhos se realize"); 
+    
+
+        }
+    }
+}
